af53f5054238db8c3adec80b46b39b2b6b34faa4
Ruby <1248276548@qq.com>
2019-10-24 03:53:53 -0700

    Add test

--- Example/test/runtests.jl
+++ Example/test/runtests.jl
@@ -1,6 +1,15 @@
 using Example
 using Test
+## One Choice
+# Example.my_f(2,1)
+# ## Another Choice, to add 'exprot' in jl
+# my_f(2,1)
+# ## safer, not export everything packages export
+# ## distinguish public between private
+# import Example
+# Example.my_f(2,1) == 7
 
 @testset "Example.jl" begin
-    # Write your own tests here.
+    @test f(2,1) == 7
+    @test f(2,3) == 13# Write your own tests here.
 end
